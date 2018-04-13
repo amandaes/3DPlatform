39e1472f14e6cf077e9fb809249412d6ddbaea8b
Amanda Ekaratih Siswojo <aes793@nyu.edu>
2018-04-13 14:29:33 -0400

    Incomplete Game. Only move and jump

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public Transform camTransform;
+
+    private Camera cam;
+    public float distanceZ = 10f;
+    public float distanceX = 10f;
+    public float distanceY = 10f;
+    private float currentX = 0f;
+    private float currentY = 0f;
+    private float sensivityX = 4f;
+    private float sensivityY = 1f;
+
+
+    // Use this for initialization
+    void Start () {
+
+        camTransform = transform;
+        cam = Camera.main;
+
+	}
+
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+    }
+
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(-distanceX, -distanceY, -distanceZ);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = target.position + dir;
+        camTransform.LookAt(target.position);
+
+     }
+
+    // Update is called once per frame
+    
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public bool touchGround;
+    public float jumpSpeed = 10f;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        touchGround = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * moveSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+
+        if (touchGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = new Vector3(0f, jumpSpeed, 0f);
+                touchGround = false;
+            }
+        }
+
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Platform"))
+        {
+            touchGround = true;
+        }
+    }
+}
